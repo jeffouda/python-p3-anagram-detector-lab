8e95326b72bac082eece34e459c58391902ce238
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-11-21 20:18:33 +0300

    test done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        matches = []
+        for w in words:
+            if sorted(w.lower()) == sorted_word:
+                matches.append(w)
+        return matches
